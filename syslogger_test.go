a4e4b7dac1bfd5c473fd27b03f4b652f6ca96693
wind85 <locci.carlo.1985@gmail.com>
2016-09-13 02:19:48 +0200

    reached over 90% coverage

--- syslogger_test.go
+++ syslogger_test.go
@@ -3,11 +3,21 @@ package syslogger
 import (
 	"log/syslog"
 	"testing"
+	"time"
 )
 
 func TestSysLogger(t *testing.T) {
 	log := NewLogger("logger-test:", syslog.LOG_ERR)
 	log.MustLog("it works!")
-	//	time.Sleep(time.Second * 1)
-	//log.Close()
+	time.Sleep(time.Second * 1)
+	log.Close()
+}
+
+func TestMsgToLong(t *testing.T) {
+	log := NewLogger("logger-test:", syslog.LOG_ERR)
+	log.MustLog("it works! to long not to long to long not to long but it is too long or it isnt't na it isn't that long :)")
+	time.Sleep(time.Second * 1)
+	log.Log("here we go")
+	time.Sleep(time.Microsecond * 500)
+	log.Close()
 }
